e4689902de43202d23b1f117e69ccb4737241930
Kineolyan <kineolyan@gmail.com>
2018-07-01 22:24:08 +0200

    Customize a bit the project.

--- src/index.js
+++ src/index.js
@@ -2,44 +2,37 @@ import xs from 'xstream';
 import Cycle from '@cycle/xstream-run';
 import {h, makeDOMDriver} from '@cycle/dom';
 require('aframe');
+require("aframe-environment-component");
 
-function main(sources) {
-  let vdom$ = xs.periodic(16).startWith(0).map(i =>
-    h('a-scene', [
-      h('a-sphere', {
+const cyclinders = (count) => {
+  let res = [];
+  for (let i = 0; i < count; i += 1) {
+    res.push(h(
+      'a-cylinder', 
+      {
         attrs: {
-          position: `0 ${i / 100} -1`,
-          radius: '1.25',
-          color: '#EF2D5E'
-        }
-      }),
-      h('a-box', {
-        attrs: {
-          position: '-1 0.5 1',
-          rotation: '0 45 0',
-          width: '1',
-          height: '1',
-          depth: '1',
-          color: '#4CC3D9'
-        }
-      }),
-      h('a-cylinder', {
-        attrs: {
-          position: '1 0.75 1',
+          position: `0 1 ${-3 - i * 1.5}`,
           radius: '0.5',
           height: '1.5',
           color: '#FFC65D'
         }
-      }),
-      h('a-plane', {
-        attrs: {
-          rotation: '-90 0 0',
-          width: '4',
-          height: '4',
-          color: '#7BC800'
-        }
-      }),
-      h('a-sky', { attrs: {color: '#ECECEC'} })
+      }));
+  }
+  return res;
+};
+
+function main(sources) {
+  let vdom$ = xs.periodic(16).startWith(0).map(i =>
+    h('a-scene', {attrs: {environment: 'preset: forest'}}, [
+      ...cyclinders(3),
+      h(
+        'a-entity',
+        {attrs: {position: '0 1 0'}},
+        [
+          h('a-camera', [
+            h('a-cursor', {attrs: {color: '#FAFAFA'}})
+          ])
+        ])
     ])
   );
 
