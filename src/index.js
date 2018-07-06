db13e02309e32c5023b9d86f43216d95d3d283ca
Kineolyan <kineolyan@gmail.com>
2018-07-06 22:24:18 +0200

    More refactoring.

--- src/index.js
+++ src/index.js
@@ -7,48 +7,7 @@ require('aframe');
 require("aframe-environment-component");
 
 import {Button, IncDecButtons} from './components/buttons.js';
-
-const cyclinders = (count) => {
-  let res = [];
-  for (let i = 0; i < count; i += 1) {
-    res.push(h(
-      'a-cylinder', 
-      {
-        attrs: {
-          position: `0 1 ${-3 - i * 1.5}`,
-          radius: '0.4',
-          height: '1.8',
-          color: '#FFC65D'
-        }
-      }));
-  }
-  return res;
-};
-
-const renderScene = (state) => h(
-  'a-scene', 
-  {
-    attrs: {
-      environment: 'preset: forest',
-      embedded: ''
-    }
-  }, 
-  [
-    ...cyclinders(state.nbPlayers),
-    h(
-      'a-entity',
-      {
-        attrs: {
-          position: `1 ${state.height} 0`,
-          rotation: '0 15 0'
-        }
-      },
-      [
-        h('a-camera', [
-          h('a-cursor', {attrs: {color: '#FAFAFA'}})
-        ])
-      ])
-  ]);
+import {renderScene} from './components/3d-vision.js';
 
 const main = (sources) => {
   const PlayerInc = isolate(IncDecButtons, 'nbPlayers');
