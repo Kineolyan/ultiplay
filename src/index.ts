fc98e2f72313c59d3db25ffcf484319099e0a39b
Kineolyan <kineolyan@gmail.com>
2018-08-09 22:26:35 +0200

    Draft of tab to refine with ant design.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import xs from 'xstream';
 import Cycle from '@cycle/xstream-run';
-import {h, div, span, button, makeDOMDriver, i} from '@cycle/dom';
+import {h, div, span, button, makeDOMDriver, i, table} from '@cycle/dom';
 import onionify from 'cycle-onionify';
 import isolate from '@cycle/isolate';
 import 'aframe';
@@ -12,10 +12,27 @@ import {Field} from './components/field';
 import Codec from './components/codec';
 import {createPlayer} from './components/players';
 
+enum Tab {
+  FIELD,
+  VISION,
+  COMBO,
+  CODEC
+};
+
+function getTabName(tab: Tab): string {
+  switch (tab) {
+  case Tab.FIELD: return 'Field';
+  case Tab.VISION: return '3D vision';
+  case Tab.COMBO: return 'Combo view';
+  case Tab.CODEC: return 'Import/Export';
+  default: throw new Error(`Unsported enum value ${tab}`);
+  }
+}
+
 const main = (sources) => {
   const initialReducer$ = xs.of(() => ({
-    nbPlayers: 2,
     height: 2,
+    tab: Tab.COMBO,
     colors: [
       '#1f77b4',
       '#ff7f0e',
@@ -28,7 +45,8 @@ const main = (sources) => {
     points: [
       createPlayer({id: 1, x: 0, y: 0}),
       createPlayer({id: 2, x: 0, y: 50})
-    ]
+    ],
+    mode: null
   }));
 
   const HeightInc = isolate(IncDecButtons, 'height');
@@ -46,8 +64,8 @@ const main = (sources) => {
   const field = isolate(Field, {onion: fieldLens})(sources);
 
   const codecLens = {
-    get: ({nbPlayers, height, points, mode, selected}) => ({
-      payload: {nbPlayers, height, points, selected},
+    get: ({height, points, mode, selected}) => ({
+      payload: {height, points, selected},
       mode
     }),
     set: (state, {mode, payload}) => {
@@ -68,27 +86,67 @@ const main = (sources) => {
     set: (state) => state
   };
   const scene = isolate(Scene, {onion: sceneLens})(sources);
+
+  const tabClick$ = sources.DOM.select('.tab').events('click')
+    .map(e => parseInt(e.srcElement.dataset['id']) as Tab);
+  const tabReducer$ = tabClick$.map(tab => state => ({...state, tab}));
   
   const reducer$ = xs.merge(
     initialReducer$,
     heightInc.onion,
     field.onion,
-    codec.onion);
+    codec.onion,
+    tabReducer$);
 
   const state$ = sources.onion.state$;
   const vdom$ = xs.combine(
+      state$,
       scene.DOM,
       heightInc.DOM,
       field.DOM,
       codec.DOM)
-    .map(([scene, heightInc, field, codec]) => div(
+    .map(([{tab}, scene, heightInc, field, codec]) => {
+      const tabElements = [];
+      switch (tab) {
+        case Tab.FIELD:
+          tabElements.push(field);
+          break;
+        case Tab.VISION:
+          tabElements.push(heightInc, scene);
+          break;
+        case Tab.COMBO:
+          tabElements.push(field, heightInc, scene);
+          break;
+        case Tab.CODEC:
+          tabElements.push(codec);
+          break;
+        default:
+          tabElements.push(div(`Unknown tab ${tab}`));
+      }
+
+      const tabs = [
+        Tab.FIELD,
+        Tab.VISION,
+        Tab.COMBO,
+        Tab.CODEC
+      ].map(t => {
+        const attrs = {
+          'data-id': t,
+          class: 'tab'
+        };
+        const name = getTabName(t);
+        return tab === t
+          ? h('li', {attrs}, [h('b', name)])
+          : h('li', {attrs}, name);
+      });
+
+      return div(
       [
         div('Small browser application to display Ultimate tactics in 3D'),
-        field,
-        heightInc,
-        scene,
-        codec
-      ]))
+        h('ul', tabs),
+        ...tabElements
+      ]);
+    })
     .replaceError(() => xs.of(div('Internal error')));
 
   return {
